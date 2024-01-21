9501d9f8c2f4065222dc918cdea95421f36b9385
Nathan Gabriel <nategabrielk@icloud.com>
2024-01-20 19:52:44 -0500

    prepared all the files for the data ingestion stage

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_ingestion import DataIngestion
+
+import pandas as pd
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data_path, test_data_path = obj.initial_data_ingestion()
+    print(train_data_path, test_data_path)
